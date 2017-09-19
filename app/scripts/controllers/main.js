a7caab7c00da7167d2d26f486192322f544b8fdb
sylvain <asylvain10@cegetel.net>
2017-09-19 14:53:13 +0200

    initializing project angular js

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testAngularjsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testAngularjsApp
+ */
+angular.module('testAngularjsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
